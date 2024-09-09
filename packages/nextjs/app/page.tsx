62791ce930cd2cbffdc3981e39539075619187b9
Jacob Homanics <homanicsjake@gmail.com>
2024-09-09 03:59:23 -0500

    Update page.tsx

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,13 +1,25 @@
 "use client";
 
-import Link from "next/link";
 import type { NextPage } from "next";
+import { optimism, sepolia } from "viem/chains";
 import { useAccount } from "wagmi";
-import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { Address } from "~~/components/scaffold-eth";
+import { useScaffoldReadContract } from "~~/hooks/scaffold-eth";
 
 const Home: NextPage = () => {
   const { address: connectedAddress } = useAccount();
+
+  const { data: result } = useScaffoldReadContract({ contractName: "YourContract2", functionName: "getValue" });
+  const { data: opResult } = useScaffoldReadContract({
+    contractName: "YourContract2",
+    functionName: "getValue",
+    chain: optimism,
+  });
+  const { data: sepoliaResult } = useScaffoldReadContract({
+    contractName: "YourContract2",
+    functionName: "getValue",
+    chain: sepolia,
+  });
 
   return (
     <>
@@ -42,24 +54,16 @@ const Home: NextPage = () => {
         <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
           <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <BugAntIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Tinker with your smart contract using the{" "}
-                <Link href="/debug" passHref className="link">
-                  Debug Contracts
-                </Link>{" "}
-                tab.
-              </p>
+              <p>User Wallet Result</p>
+              {result}
             </div>
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Explore your local transactions with the{" "}
-                <Link href="/blockexplorer" passHref className="link">
-                  Block Explorer
-                </Link>{" "}
-                tab.
-              </p>
+              <p>Sepolia Result</p>
+              {sepoliaResult}
+            </div>
+            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
+              <p>OP Mainnet Result</p>
+              {opResult}
             </div>
           </div>
         </div>
